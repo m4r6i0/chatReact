a897175fb9249c37eb84c38c628187506f9db16c
m4r6i0 <m4r6i0@gmail.com>
2022-04-07 09:12:15 -0300

    resolve cors 666

--- server.js
+++ server.js
@@ -1,44 +1,52 @@
+// const { response } = require('express')
+// const express = require('express')
+// const cors = require('cors');
+
+// const app = express()
+
+
+// app.use(express.static("public"));
+// app.use(cors());
+
+// const http = require('http').Server(app)
+// const serverSocket = require('socket.io')(http)
+
+// const porta = process.env.PORT || 8000
+
+// const host = process.env.HEROKU_APP_NAME ? `https://${process.env.HEROKU_APP_NAME}.herokuapp.com` : "http://localhost"
+// console.log(host);
+
+// const server = serverSocket(http, {
+//     cors:{
+//         origin: "*",
+//         methods: ["GET", "POST"],
+//         allowedHeaders: ["content-type"],
+//     },
+// });
+
 const { response } = require('express')
 const express = require('express')
-const cors = require('cors');
+const cors = require("cors");
+const socketio = require("socket.io");
 
-const app = express()
+const app = express();
+app.use(cors());
+app.use(express.static("public"));
 
-// app.use(cors());
-// app.options('*', cors());
-// app.use(express.static("public"));
+const server = require('http').Server(app);
 
-if (process.env.NODE_ENV !== "production") {
-    require("dotenv").config()
-  }
-  
-  const domainsFromEnv = process.env.CORS_DOMAINS || ""
-  
-  const whitelist = domainsFromEnv.split(",").map(item => item.trim())
-  console.log(whitelist);
-
-  const corsOptions = {
-    origin: function (origin, callback) {
-      if (!origin || whitelist.indexOf(origin) !== -1) {
-        callback(null, true)
-      } else {
-        callback(new Error("Not allowed by CORS"))
-      }
-    },
-    credentials: true,
-  }
-  app.use(cors(corsOptions))
-  app.use(express.static("public"));
-
-
-const http = require('http').Server(app)
-const serverSocket = require('socket.io')(http)
+const socket = socketio(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["content-type"],
+  },
+});
 
 const porta = process.env.PORT || 8000
-
 const host = process.env.HEROKU_APP_NAME ? `https://${process.env.HEROKU_APP_NAME}.herokuapp.com` : "http://localhost"
 
-http.listen(porta, function(){
+server.listen(porta, function(){
     const portaStr = porta === 80 ? '' :  ':' + porta
 
     if (process.env.HEROKU_APP_NAME) 
@@ -51,7 +59,7 @@ app.get('/', function (requisicao, resposta) {
 })
 
 
-serverSocket.on('connect', function(socket){
+socket.on('connect', function(socket){
     socket.on('login', function (nickname) {
         socket.nickname = nickname
         const msg = nickname + ' conectou'
