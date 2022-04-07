3c201e1ed36f45d2e5a985877c737fa190c1529f
m4r6i0 <m4r6i0@gmail.com>
2022-04-07 15:11:40 -0300

    collect messagens and cors

--- server.js
+++ server.js
@@ -1,81 +1,60 @@
-// const { response } = require('express')
-// const express = require('express')
-// const cors = require('cors');
-
-// const app = express()
-
-
-// app.use(express.static("public"));
-// app.use(cors());
-
-// const http = require('http').Server(app)
-// const serverSocket = require('socket.io')(http)
-
-// const porta = process.env.PORT || 8000
-
-// const host = process.env.HEROKU_APP_NAME ? `https://${process.env.HEROKU_APP_NAME}.herokuapp.com` : "http://localhost"
-// console.log(host);
-
-// const server = serverSocket(http, {
-//     cors:{
-//         origin: "*",
-//         methods: ["GET", "POST"],
-//         allowedHeaders: ["content-type"],
-//     },
-// });
-
-const { response } = require('express')
-const express = require('express')
-const cors = require("cors");
-const socketio = require("socket.io");
-
+const express = require("express");
+const socket = require("socket.io");
 const app = express();
-app.use(cors());
-app.use(express.static("public"));
+const cors = require("cors");
+const res = require("express/lib/response");
+const { json } = require("express/lib/response");
 
-const server = require('http').Server(app);
+app.use(cors({ origin: true }));
+app.use(express.json());
 
-const socket = socketio(server, {
-  cors: {
-    origin: "http://localhost:3001",
-  },
-});
+class MensagemPayload {
+    constructor(nome, mensgem){
+        this.nome = nome;
+        this.mensagem = mensgem;
+    }
+}
 
-const porta = process.env.PORT || 8000
+let listaMensagem = [];
+
+
+const port = process.env.PORT || 8000
 const host = process.env.HEROKU_APP_NAME ? `https://${process.env.HEROKU_APP_NAME}.herokuapp.com` : "http://localhost"
 
-server.listen(porta, function(){
-    const portaStr = porta === 80 ? '' :  ':' + porta
+const server = app.listen(port, () => {
+      const portStr = port === 80 ? '' :  ':' + port
+      if (process.env.HEROKU_APP_NAME) 
+          console.log('Servidor iniciado. Abra o navegador em ' + host)
+      else 
+            console.log('Servidor iniciado. Abra o navegador em ' + host + portStr)  
+});
 
-    if (process.env.HEROKU_APP_NAME) 
-        console.log('Servidor iniciado. Abra o navegador em ' + host)
-    else console.log('Servidor iniciado. Abra o navegador em ' + host + portaStr)
-})
+io = socket(server, { 
+    cors: { 
+        origin: ["https://my-frontend.com", "http://localhost:3001", "http://localhost:3000"],
+        methods: ["GET", "POST"],
+        credentials: true
+    },
+    allowEIO3: true
+});
 
 app.get('/', function (requisicao, resposta) {
-    resposta.send({message: "Server On Cors-10"});
-})
+    resposta.sendStatus(200);
+});
 
 
-socket.on('connect', function(socket){
-    socket.on('login', function (nickname) {
-        socket.nickname = nickname
-        const msg = nickname + ' conectou'
-        console.log(msg)
-        serverSocket.emit('chat msg', msg)
-    })
+io.on("connection", (socket) => {
+  console.log(socket.id);
 
-    socket.on('disconnect', function(){
-        console.log('Cliente desconectado: ' + socket.nickname)
-    })
-        
-    socket.on('chat msg', function(msg){
-        serverSocket.emit('chat msg', `${socket.nickname} diz: ${msg}`)
-        console.log('Mensagem ->' + msg);
-    })
+socket.on("send_message", (data) => {
 
-    socket.on('status', function(msg){
-        console.log(msg)
-        socket.broadcast.emit('status', msg)
-    })
-})
+    let mensagem = new MensagemPayload(data.nome, data.mensagem);
+    listaMensagem.push(mensagem);
+    socket.emit("receive_message", listaMensagem);
+    console.log(listaMensagem);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("USER DISCONNECTED");
+  });
+});
